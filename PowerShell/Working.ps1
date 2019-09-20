7676f4f8d764149ba8ccbbea29dd030b430400ff
Bob Stobie <stobib@bsnepc.us>
2019-09-20 02:28:11 -0500

    Working on sudo su thru Posh-SSH

---
+++ PowerShell/Working.ps1
@@ -0,0 +1,30 @@
+﻿Clear-Host;Clear-History
+Import-Module ProcessCredentials
+Import-Module Posh-SSH
+$Global:Domain=("utshare.local")
+$Global:DomainUser=(($env:USERNAME+"@"+$Domain).ToLower())
+Switch($DomainUser){
+    {($_-like"sy10*")-or($_-like"sy60*")}{Break}
+    Default{$DomainUser=(("sy1000829946@"+$Domain).ToLower());Break}
+}
+$SecureCredentials=SetCredentials -SecureUser $DomainUser -Domain ($Domain).Split(".")[0]
+If(!($SecureCredentials)){$SecureCredentials=get-credential}
+$secpas=$SecureCredentials.Password
+$ServerList=@("arcsdevibz01.dev.utshare.local")
+$Command=("ls -al /home/"+($DomainUser).Split("@")[0])
+Foreach($FQDN In $ServerList){
+    $Results=$null
+    $SessionID=New-SSHSession -ComputerName $FQDN -Credential $SecureCredentials
+    $Results=Invoke-SSHCommand -Index $SessionID.sessionid -Command $Command
+    If($Results.ExitStatus-eq0){
+        Write-Host("`tSuccessfully connected to "+$FQDN+" using [Posh-SSH].")
+    }ElseIf($Results.ExitStatus-eq2){
+        $stream = $SessionID.Session.CreateShellStream("PS-SSH", 0, 0, 0, 0, 100)
+        $SSHusersName = ($DomainUser).Split("@")[0].Trim()
+        $Command=("chown "+$SSHusersName+" /home/"+$SSHusersName+"/ -R")
+        $results = Invoke-SSHStreamExpectSecureAction -ShellStream $stream -Command ("sudo su - "+$Command) -ExpectString "[sudo] password for $($SSHusersName):" -SecureAction $secpas
+        Write-Host $Results
+    }Else{
+        Write-Host $Results
+    }
+}
