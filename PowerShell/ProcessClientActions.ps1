12aa53b537f070cfa404a138c1860c1823c464f2
Bob W Stobie <bstobie@utsystem.edu>
2019-11-15 15:08:12 -0600

    Modified script to process multiple computers

--- PowerShell/ProcessClientActions.ps1
+++ PowerShell/ProcessClientActions.ps1
@@ -1,90 +1,89 @@
-﻿Function RunClientAction{[CmdletBinding()]param(
-    [Parameter(Position=0,Mandatory=$True,HelpMessage="Provide server names",ValueFromPipeline=$true)][string[]]$Computers,
-    [ValidateSet('HardwareInventory','SoftwareInventory','DataDiscoveryRecord','FileCollection','IDMIFCollection','ClientMachineAuthentication','MachinePolicyAssignmentsRequest','MachinePolicyEvaluation',
-    'RefreshDefaultMPTask','LSRefreshLocationsTask','LSTimeoutRefreshTask','PolicyAgentRequestAssignment','PolicyAgentEvaluateAssignment','SoftwareMeteringGeneratingUsageReport','SourceUpdateMessage',
-    'Clearingproxysettingscache','MachinePolicyAgentCleanup','UserPolicyAgentCleanup','PolicyAgentValidateMachinePolicy','PolicyAgentValidateUserPolicy','RefreshingcertificatesinADonMP','PeerDPStatusreporting',
-    'PeerDPPendingpackagecheckschedule','SUMUpdatesinstallschedule','HardwareInventoryCollectionCycle','SoftwareInventoryCollectionCycle','DiscoveryDataCollectionCycle','FileCollectionCycle','IDMIFCollectionCycle',
-    'SoftwareMeteringUsageReportCycle','WindowsInstallerSourceListUpdateCycle','SoftwareUpdatesAssignmentsEvaluationCycle','BranchDistributionPointMaintenanceTask','SendUnsentStateMessage',
-    'StateSystempolicycachecleanout','ScanbyUpdateSource','UpdateStorePolicy','Statesystempolicybulksendhigh','Statesystempolicybulksendlow','Applicationmanagerpolicyaction','Applicationmanageruserpolicyaction',
-    'Applicationmanagerglobalevaluationaction','Powermanagementstartsummarizer','Endpointdeploymentreevaluate','EndpointAMpolicyreevaluate','Externaleventdetection')]
-    [string[]]$ClientAction
-)
+﻿Clear-Host;Clear-History
+Import-Module ProcessCredentials
+$Global:SecureCredentials=$null
+$Global:Domain=("utshare.local")
+$Global:DomainUser=(($env:USERNAME+"@"+$Domain).ToLower())
+$ScriptPath=$MyInvocation.MyCommand.Definition
+$ScriptName=$MyInvocation.MyCommand.Name
+$ErrorActionPreference='SilentlyContinue'
+Set-Location ($ScriptPath.Replace($ScriptName,""))
+Set-Variable -Name SecureCredentials -Value $null
+Set-Variable -Name LogName -Value ($ScriptName.Replace("ps1","log"))
+Set-Variable -Name LogFile -Value ($env:USERPROFILE+"\Desktop\"+$LogName)
+Set-Variable -Name InputName -Value ($ScriptName.Replace("ps1","txt"))
+Set-Variable -Name InputFile -Value ($env:USERPROFILE+"\Desktop\"+$InputName)
+Function RunSCCMClientAction{[CmdletBinding()]param(
+    [Parameter(Position=0, Mandatory=$true,HelpMessage="Provide server names",ValueFromPipeline=$true)][string[]]$Computername,
+    [ValidateSet('MachinePolicy','DiscoveryData','AppDeployment','HardwareInventory','UpdateDeployment','UpdateScan','SoftwareInventory')][string[]]$ClientAction)
     $ActionResults=@()
     Try{
-        ForEach($ComputerName In $Computers){
-            $ComputerName=($ComputerName+".vdi.utshare.local")
-            $ActionResults=Invoke-Command -ComputerName $Computername{param($ClientAction)} -ArgumentList $ClientAction -ErrorAction Stop | Select-Object @{n='Computer Name';e={$_.pscomputername}},"Action name",Status
-                Foreach($Item in $ClientAction){
-                    $Object=@{} | Select-Object "Action name",Status
-                    Try{
-                        $ScheduleIDMappings=@{ 
-                            'HardwareInventory'                            = '{00000000-0000-0000-0000-000000000001}'
-                            'SoftwareInventory'                            = '{00000000-0000-0000-0000-000000000002}'
-                            'DataDiscoveryRecord'                          = '{00000000-0000-0000-0000-000000000003}'
-                            'FileCollection'                               = '{00000000-0000-0000-0000-000000000010}'
-                            'IDMIFCollection'                              = '{00000000-0000-0000-0000-000000000011}'
-                            'ClientMachineAuthentication'                  = '{00000000-0000-0000-0000-000000000012}'
-                            'MachinePolicyAssignmentsRequest'              = '{00000000-0000-0000-0000-000000000021}'
-                            'MachinePolicyEvaluation'                      = '{00000000-0000-0000-0000-000000000022}'
-                            'RefreshDefaultMPTask'                         = '{00000000-0000-0000-0000-000000000023}'
-                            'LSRefreshLocationsTask'                       = '{00000000-0000-0000-0000-000000000024}'
-                            'LSTimeoutRefreshTask'                         = '{00000000-0000-0000-0000-000000000025}'
-                            'PolicyAgentRequestAssignment'                 = '{00000000-0000-0000-0000-000000000026}'
-                            'PolicyAgentEvaluateAssignment'                = '{00000000-0000-0000-0000-000000000027}'
-                            'SoftwareMeteringGeneratingUsageReport'        = '{00000000-0000-0000-0000-000000000031}'
-                            'SourceUpdateMessage'                          = '{00000000-0000-0000-0000-000000000032}'
-                            'Clearingproxysettingscache'                   = '{00000000-0000-0000-0000-000000000037}'
-                            'MachinePolicyAgentCleanup'                    = '{00000000-0000-0000-0000-000000000040}'
-                            'UserPolicyAgentCleanup'                       = '{00000000-0000-0000-0000-000000000041}'
-                            'PolicyAgentValidateMachinePolicy'             = '{00000000-0000-0000-0000-000000000042}'
-                            'PolicyAgentValidateUserPolicy'                = '{00000000-0000-0000-0000-000000000043}'
-                            'RefreshingcertificatesinADonMP'               = '{00000000-0000-0000-0000-000000000051}'
-                            'PeerDPStatusreporting'                        = '{00000000-0000-0000-0000-000000000061}'
-                            'PeerDPPendingpackagecheckschedule'            = '{00000000-0000-0000-0000-000000000062}'
-                            'SUMUpdatesinstallschedule'                    = '{00000000-0000-0000-0000-000000000063}'
-                            'HardwareInventoryCollectionCycle'             = '{00000000-0000-0000-0000-000000000101}'
-                            'SoftwareInventoryCollectionCycle'             = '{00000000-0000-0000-0000-000000000102}'
-                            'DiscoveryDataCollectionCycle'                 = '{00000000-0000-0000-0000-000000000103}'
-                            'FileCollectionCycle'                          = '{00000000-0000-0000-0000-000000000104}'
-                            'IDMIFCollectionCycle'                         = '{00000000-0000-0000-0000-000000000105}'
-                            'SoftwareMeteringUsageReportCycle'             = '{00000000-0000-0000-0000-000000000106}'
-                            'WindowsInstallerSourceListUpdateCycle'        = '{00000000-0000-0000-0000-000000000107}'
-                            'SoftwareUpdatesAssignmentsEvaluationCycle'    = '{00000000-0000-0000-0000-000000000108}'
-                            'BranchDistributionPointMaintenanceTask'       = '{00000000-0000-0000-0000-000000000109}'
-                            'SendUnsentStateMessage'                       = '{00000000-0000-0000-0000-000000000111}'
-                            'StateSystempolicycachecleanout'               = '{00000000-0000-0000-0000-000000000112}'
-                            'ScanbyUpdateSource'                           = '{00000000-0000-0000-0000-000000000113}'
-                            'UpdateStorePolicy'                            = '{00000000-0000-0000-0000-000000000114}'
-                            'Statesystempolicybulksendhigh'                = '{00000000-0000-0000-0000-000000000115}'
-                            'Statesystempolicybulksendlow'                 = '{00000000-0000-0000-0000-000000000116}'
-                            'Applicationmanagerpolicyaction'               = '{00000000-0000-0000-0000-000000000121}'
-                            'Applicationmanageruserpolicyaction'           = '{00000000-0000-0000-0000-000000000122}'
-                            'Applicationmanagerglobalevaluationaction'     = '{00000000-0000-0000-0000-000000000123}'
-                            'Powermanagementstartsummarizer'               = '{00000000-0000-0000-0000-000000000131}'
-                            'Endpointdeploymentreevaluate'                 = '{00000000-0000-0000-0000-000000000221}'
-                            'EndpointAMpolicyreevaluate'                   = '{00000000-0000-0000-0000-000000000222}'
-                            'Externaleventdetection'                       = '{00000000-0000-0000-0000-000000000223}'
-                        }
-                        $ScheduleID=$ScheduleIDMappings[$item]
-                        Write-Verbose "Processing $Item - $ScheduleID"
-                        [void]([wmiclass] "root\ccm:SMS_Client").TriggerSchedule($ScheduleID);
-                        $Status="Success"
-                        Write-Verbose "Operation status - $status"
-                    }
-                    Catch{
-                        $Status="Failed"
-                        Write-Verbose "Operation status - $status"
-                    }
-                    $Object."Action name"=$item
-                    $Object.Status=$Status
-                    $Object
-                }
+        ForEach($Item In $ClientAction){
+            $Status=$null
+            Switch($Item){
+                "MachinePolicy"{$TriggerID='{00000000-0000-0000-0000-000000000021}';Break}
+                "DiscoveryData"{$TriggerID='{00000000-0000-0000-0000-000000000003}';Break}
+                "AppDeployment"{$TriggerID='{00000000-0000-0000-0000-000000000121}';Break}
+                "HardwareInventory"{$TriggerID='{00000000-0000-0000-0000-000000000001}';Break}
+                "UpdateDeployment"{$TriggerID='{00000000-0000-0000-0000-000000000108}';Break}
+                "UpdateScan"{$TriggerID='{00000000-0000-0000-0000-000000000113}';Break}
+                "SoftwareInventory"{$TriggerID='{00000000-0000-0000-0000-000000000002}';Break}
             }
+            $Object=@{} | Select-Object "Action name",Status
+            Try{
+                $ScheduleID=($Item+" - "+$TriggerID)
+                Write-Verbose("Processing '"+$ScheduleID+"'")
+                $Return=Invoke-WMIMethod -ComputerName $Computername -Namespace root\ccm -Class SMS_CLIENT -Name TriggerSchedule $TriggerID
+                If($Return.ReturnValue-eq""){
+                    $Status="Started"
+                    $Color="Yellow"
+                }Else{
+                    $Status="Success"
+                    $Color="DarkCyan"
+                }
+            }Catch{
+                $Status="Failed"
+                $Color="Red"
+            }
+            Write-Verbose("Operation status - "+$Status)
+            $Object."Action name"=$Item
+            $Object.Status=$Status
+            Write-Host("`t`t"+$Object) -ForegroundColor $Color
         }
-    Catch{
+    }Catch{
         Write-Error $_.Exception.Message
     }
-    Return $ActionResults
+    Return("`t"+$ActionResults)
 }
-#RunClientAction -Computername $env:ComputerName -ClientAction 'HardwareInventory','SoftwareInventory','ScanbyUpdateSource','SoftwareUpdatesAssignmentsEvaluationCycle'
-RunClientAction -Computers (Get-Content .\VMExport\InputFile.txt) -ClientAction 'HardwareInventory','SoftwareInventory','ScanbyUpdateSource','SoftwareUpdatesAssignmentsEvaluationCycle'
+Switch($DomainUser){
+    {($_-like"sy10*")-or($_-like"sy60*")}{Break}
+    Default{$DomainUser=(("sy1000829946@"+$Domain).ToLower());Break}
+}
+$SecureCredentials=SetCredentials -SecureUser $DomainUser -Domain ($Domain).Split(".")[0]
+If(!($SecureCredentials)){$SecureCredentials=get-credential}
+If(Test-Path -Path $InputFile){
+    $ServiceName="SMS Agent Host"
+    ForEach($ComputerName In [System.IO.File]::ReadLines($InputFile)){
+        $Computer=(Get-ADComputer -Identity "$ComputerName" -Properties DNSHostName).DNSHostName
+        $Status=(Test-NetConnection $Computer).PingSucceeded
+        If($Status-eq$true){
+            Write-Host("Checking '"+$Computer+"' for status of '"+$ServiceName+"' service.")
+            $Running=Get-Service -Name $ServiceName -ComputerName $Computer
+            If($Running.Status -eq "Running"){
+                $ServiceStatus="Running"
+                Write-Host("`t"+$Computer+"`t`t"+$ServiceStatus) -ForegroundColor Green
+                RunSCCMClientAction -Computername $Computer -ClientAction 'HardwareInventory','SoftwareInventory','UpdateScan','UpdateDeployment'
+            }ElseIf($Running.Status -eq "Stopped"){ 
+                $ServiceStatus="Not Running"
+                Write-Host("`t"+$Computer+"`t`t"+$ServiceStatus) -ForegroundColor Yellow
+            }Else{
+                $ServiceStatus="Not Installed"
+                Write-Host("`t"+$Computer+"`t`t"+$ServiceStatus) -ForegroundColor Red
+            }
+        }Else{
+            Write-Host("Connection to '"+$Computer+"' failed.") -ForegroundColor Yellow
+        }
+    }
+}Else{
+    RunSCCMClientAction -Computername $env:ComputerName -ClientAction 'HardwareInventory','SoftwareInventory','UpdateScan','UpdateDeployment'
+}
+Set-Location ($env:SystemRoot+"\System32")
