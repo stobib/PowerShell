91f236d18426479aac07c80e6ad33b91002fe420
Bob W Stobie <bstobie@utsystem.edu>
2019-11-19 06:47:39 -0600

    Added logging to the client action push script

--- PowerShell/ProcessClientActions.ps1
+++ PowerShell/ProcessClientActions.ps1
@@ -1,17 +1,22 @@
 ﻿Clear-Host;Clear-History
-Import-Module ProcessCredentials
-$Global:SecureCredentials=$null
-$Global:Domain=("utshare.local")
-$Global:DomainUser=(($env:USERNAME+"@"+$Domain).ToLower())
+$Global:Message=$null
 $ScriptPath=$MyInvocation.MyCommand.Definition
 $ScriptName=$MyInvocation.MyCommand.Name
 $ErrorActionPreference='SilentlyContinue'
 Set-Location ($ScriptPath.Replace($ScriptName,""))
-Set-Variable -Name SecureCredentials -Value $null
-Set-Variable -Name LogName -Value ($ScriptName.Replace("ps1","log"))
-Set-Variable -Name LogFile -Value ($env:USERPROFILE+"\Desktop\"+$LogName)
+Set-Variable -Name LogFolder -Value ($ScriptName.Replace(".ps1",""))
+Set-Variable -Name LogFolderPath -Value ($env:USERPROFILE+"\Desktop\"+$LogFolder)
+Set-Variable -Name LogName -Value ($LogFolder+".log")
+$Global:LogFile=($LogFolderPath+"\"+$LogName)
 Set-Variable -Name InputName -Value ($ScriptName.Replace("ps1","txt"))
-Set-Variable -Name InputFile -Value ($env:USERPROFILE+"\Desktop\"+$InputName)
+Set-Variable -Name InputFile -Value ($LogFolderPath+"\"+$InputName)
+Set-Variable -Name NotRunningName -Value ("SMS_Not_Running.log")
+Set-Variable -Name NotRunningFile -Value ($LogFolderPath+"\"+$NotRunningName)
+Set-Variable -Name NotInstalledName -Value ("SMS_Not_Installed.log")
+Set-Variable -Name NotInstalledFile -Value ($LogFolderPath+"\"+$NotInstalledName)
+Set-Variable -Name OfflineName -Value ("Systems_Offline.log")
+Set-Variable -Name OfflineFile -Value ($LogFolderPath+"\"+$OfflineName)
+$Script:ClearLogs=@($LogFile,$NotRunningFile,$NotInstalledFile,$OfflineFile)
 Function RunSCCMClientAction{[CmdletBinding()]param(
     [Parameter(Position=0, Mandatory=$true,HelpMessage="Provide server names",ValueFromPipeline=$true)][string[]]$Computername,
     [ValidateSet('MachinePolicy','DiscoveryData','AppDeployment','HardwareInventory','UpdateDeployment','UpdateScan','SoftwareInventory')][string[]]$ClientAction)
@@ -28,7 +33,7 @@ Function RunSCCMClientAction{[CmdletBinding()]param(
                 "UpdateScan"{$TriggerID='{00000000-0000-0000-0000-000000000113}';Break}
                 "SoftwareInventory"{$TriggerID='{00000000-0000-0000-0000-000000000002}';Break}
             }
-            $Object=@{} | Select-Object "Action name",Status
+            $Object=@{}|Select-Object "Action name",Status
             Try{
                 $ScheduleID=($Item+" - "+$TriggerID)
                 Write-Verbose("Processing '"+$ScheduleID+"'")
@@ -47,40 +52,58 @@ Function RunSCCMClientAction{[CmdletBinding()]param(
             Write-Verbose("Operation status - "+$Status)
             $Object."Action name"=$Item
             $Object.Status=$Status
-            Write-Host("`t`t"+$Object) -ForegroundColor $Color
+            $Message=("`t`t"+$Object)
+            ($Message)|Out-File $LogFile -Append
+            $Message|Write-Host -ForegroundColor $Color
         }
     }Catch{
         Write-Error $_.Exception.Message
     }
     Return("`t"+$ActionResults)
 }
-Switch($DomainUser){
-    {($_-like"sy10*")-or($_-like"sy60*")}{Break}
-    Default{$DomainUser=(("sy1000829946@"+$Domain).ToLower());Break}
+ForEach($CurrentLog In $ClearLogs){
+    If(Test-Path -Path $CurrentLog){
+        Remove-Item $CurrentLog
+    }
+    ("Beginning new log file for "+$LogFolder+".")|Out-File $CurrentLog
+    Get-Date -Format "dddd MM/dd/yyyy HH:mm K"|Out-File $CurrentLog -Append
+    If($CurrentLog-ne$LogFile){
+        $Header="Computer Name"
+        $Line=("_"*$Header.Length)
+        ("`r`t"+$Header)|Out-File $CurrentLog -Append
+        ("`t"+$Line)|Out-File $CurrentLog -Append
+    }
 }
-$SecureCredentials=SetCredentials -SecureUser $DomainUser -Domain ($Domain).Split(".")[0]
-If(!($SecureCredentials)){$SecureCredentials=get-credential}
 If(Test-Path -Path $InputFile){
     $ServiceName="SMS Agent Host"
     ForEach($ComputerName In [System.IO.File]::ReadLines($InputFile)){
+        $ComputerName=($ComputerName.Split("."))[0]
         $Computer=(Get-ADComputer -Identity "$ComputerName" -Properties DNSHostName).DNSHostName
         $Status=(Test-NetConnection $Computer).PingSucceeded
         If($Status-eq$true){
-            Write-Host("Checking '"+$Computer+"' for status of '"+$ServiceName+"' service.")
+            $Message=("Checking '"+$Computer+"' for status of '"+$ServiceName+"' service.")
+            ("`t"+$Message)|Out-File $LogFile -Append
+            $Message|Write-Host -ForegroundColor Cyan
             $Running=Get-Service -Name $ServiceName -ComputerName $Computer
             If($Running.Status -eq "Running"){
                 $ServiceStatus="Running"
-                Write-Host("`t"+$Computer+"`t`t"+$ServiceStatus) -ForegroundColor Green
-                RunSCCMClientAction -Computername $Computer -ClientAction 'HardwareInventory','SoftwareInventory','UpdateScan','UpdateDeployment'
+                $Message=("`t"+$Computer+"`t`t"+$ServiceStatus)
+                ($Message)|Out-File $LogFile -Append
+                $Message|Write-Host -ForegroundColor Green
+                $Message=RunSCCMClientAction -Computername $Computer -ClientAction 'HardwareInventory','SoftwareInventory','UpdateScan','UpdateDeployment'
             }ElseIf($Running.Status -eq "Stopped"){ 
-                $ServiceStatus="Not Running"
-                Write-Host("`t"+$Computer+"`t`t"+$ServiceStatus) -ForegroundColor Yellow
+                $Message=("`t"+$Computer)
+                ($Message)|Out-File $NotRunningFile -Append
+                $Message|Write-Host -ForegroundColor Yellow
             }Else{
-                $ServiceStatus="Not Installed"
-                Write-Host("`t"+$Computer+"`t`t"+$ServiceStatus) -ForegroundColor Red
+                $Message=("`t"+$Computer)
+                ($Message)|Out-File $NotInstalledFile -Append
+                $Message|Write-Host -ForegroundColor Red
             }
         }Else{
-            Write-Host("Connection to '"+$Computer+"' failed.") -ForegroundColor Yellow
+            $Message=("`t"+$Computer)
+            ($Message)|Out-File $OfflineFile -Append
+            $Message|Write-Host -ForegroundColor Magenta
         }
     }
 }Else{
