b56c85269bc41ab286e599402cb4c6658511c0d5
Bob W Stobie <bstobie@utsystem.edu>
2020-01-15 08:13:54 -0600

    Configured for Mass Add

--- PowerShell/UpdateDNS.ps1
+++ PowerShell/UpdateDNS.ps1
@@ -3,7 +3,7 @@ Import-Module ProcessCredentials
 $Global:ResetHost=@()
 $Global:SecureCredentials=$null
 $Global:Domain=("utshare.local")
-$Global:DNSServer=("dcb01."+$Domain).ToLower()
+$Global:DNSServer=("dca01."+$Domain).ToLower()
 $Global:DomainUser=(($env:USERNAME+"@"+$Domain).ToLower())
 $ScriptPath=$MyInvocation.MyCommand.Definition
 $ScriptName=$MyInvocation.MyCommand.Name
@@ -20,10 +20,14 @@ Function ChangesDNSRecords{Param([Parameter(Mandatory=$True)]$Hostname,[IPAddres
     $Zone=($Hostname.ToString().Split(".")[1]+"."+$Domain)
     $Ptr=($IPAddress.IPAddressToString).Split(".")[3]
     $Subptr=($IPAddress.IPAddressToString).Split(".")[2]
-#    $ZonePtr=($Subptr+".118.10.in-addr.arpa")
-    $ZonePtr=($Subptr+".126.10.in-addr.arpa")
+    If($IPAddress.IPAddressToString-like"*.118.*"){
+        $ZonePtr=($Subptr+".118.10.in-addr.arpa")
+    }Else{
+        $ZonePtr=($Subptr+".126.10.in-addr.arpa")
+    }
     Try{
-        Remove-DnsServerResourceRecord -ComputerName $DNSServer -ZoneName $Zone -RRType A -Name $NetBIOS -RecordData $AddressList.IPAddressToString -Force
+<#        Mass update of missing ESXi Hosts
+        Remove-DnsServerResourceRecord -ComputerName $DNSServer -ZoneName $Zone -RRType A -Name $NetBIOS -RecordData $AddressList.IPAddressToString -Force        #>
         Add-DnsServerResourceRecordA -ComputerName $DNSServer -Name $NetBIOS -ZoneName $Zone -AllowUpdateAny -IPv4Address $IPAddress -TimeToLive 01:00:00
         Add-DnsServerResourceRecordPtr -ComputerName $DNSServer -Name $Ptr -ZoneName $ZonePtr -AllowUpdateAny -TimeToLive 01:00:00 -PtrDomainName $Hostname
     }Catch{
@@ -34,12 +38,16 @@ Switch($DomainUser){
     Default{$DomainUser=(("sy1000829946@"+$Domain).ToLower());Break}
 }
 $SecureCredentials=SetCredentials -SecureUser $DomainUser -Domain ($Domain).Split(".")[0]
-If(!($SecureCredentials)){$SecureCredentials=get-credential}
+If(!($SecureCredentials)){$SecureCredentials=Get-Credential}
 If(Test-Path -Path $InputFile){
     ForEach($ServerInfo In [System.IO.File]::ReadLines($InputFile)){
+<#        Mass update of missing ESXi Hosts        #>
+        $FQDN=($ServerInfo.Split(",")[0]+".esx."+$Domain)
+        $IPv4=($ServerInfo.Split(",")[1])
+<#        Mass update of missing ESXi Hosts
         $FQDN=$ServerInfo.Split(",")[0]
-#        $IPv4=($ServerInfo.Split(",")[1].Replace("126","118"))
         $IPv4=($ServerInfo.Split(",")[1].Replace("118","126"))
+#>
         ChangesDNSRecords -Hostname $FQDN -IPAddress $IPv4
     }
 }
