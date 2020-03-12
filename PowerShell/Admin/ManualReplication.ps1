d033b770bb5d6a699c200061a8aaff9365f040e4
Bob W Stobie <bstobie@utsystem.edu>
2020-03-12 08:03:24 -0500

    Security Update

---
+++ PowerShell/Admin/ManualReplication.ps1
@@ -0,0 +1,10 @@
+﻿Clear-History;Clear-Host
+Set-Variable -Name RGName -Value "Domain System Volume"
+Set-Variable -Name LogOnServer -Value $env:LOGONSERVER
+Set-Variable -Name DNSDomain -Value $env:USERDNSDOMAIN
+# $DCList=((Get-ADForest).Domains|%{Get-ADDomainController -Filter * -Server $_}).HostName
+$DomainControllers=($DNSDomain|%{Get-ADDomainController -Filter * -Server $_}).HostName
+ForEach($DC In $DomainControllers){
+    DFSRDiag SyncNow /Partner:$DC /rgname:$RGName /time:15
+}
+
