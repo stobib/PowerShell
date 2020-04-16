ea67f5b815d77a4cb23f18ac30094713993035b6
Bob W Stobie <bstobie@utsystem.edu>
2020-04-16 11:41:02 -0500

    Initial Script for PowerState

--- PowerShell/Services/Change_Service_windows_workstations.ps1
+++ PowerShell/Services/Change_Service_windows_workstations.ps1
@@ -8,6 +8,7 @@ $Global:PShell="C:\Windows\System32\WindowsPowerShell\v1.0\PowerShell.exe"
 Set-Variable -Name OutputFile -Value ($env:TEMP+"\WindowsWorkstations.txt")
 Set-Variable -Name SystemDivide -Value "-------------------------------------------------------------------------------"
 Get-ADComputer -Filter{(OperatingSystem -like "Windows 10*")-or(OperatingSystem -like "Windows 8.*")} -Properties Name,IPv4Address,OperatingSystem|Sort-Object Name|Select-Object DNSHostName,IPv4Address,OperatingSystem|Out-File $OutputFile
+#Get-ADComputer -Filter{DNSHostName -like "win10admy001.*"} -Properties Name,IPv4Address,OperatingSystem|Sort-Object Name|Select-Object DNSHostName,IPv4Address,OperatingSystem|Out-File $OutputFile
 $HostnameList=(Get-Content -Path $OutputFile)
 Switch($DomainUser){
     {($_-like"sy100*")-or($_-like"sy600*")}{Break}
@@ -57,13 +58,10 @@ ForEach($Hostname In $HostnameList){
                 }
                 $Counter=0
                 ForEach($Row In $RCHeaders){
-#                    Write-Host ("RoboCopy "+$Row.Source+" "+$Row.Destination+" /R:0 /W:0 > "+$env:temp+"\"+$Name.Split(".")[0]+"_"+$Counter+".tmp")
                     RoboCopy $Row.Source $Row.Destination /R:0 /W:0 > ($env:temp+"\"+$Name.Split(".")[0]+"_"+$Counter+".tmp")
                     $Destination=($Row.Destination.Replace("""","")+"\"+$ScriptName)
                     $Counter++
                 }
-#                Pause
-#<#
                 $DeletiontDate=$CurrentDate.AddDays($OldFiles)
                 PSexec.exe ("\\"+$Name) cmd.exe /C ("ForFiles /P %SystemDrive%\users\ > %SystemDrive%\Scripts\Users.tmp")
                 [System.Collections.ArrayList]$PathForDeletion=@()
@@ -86,40 +84,81 @@ ForEach($Hostname In $HostnameList){
                 }Until($Counter-eq2)
                 If(Test-Path -Path $Destination){
                     Try{
-<#
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "fsutil 8dot3name set 1"
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "$PShell Set-ExecutionPolicy -Scope CurrentUser Unrestricted"
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "$PShell Set-TimeZone -Name 'Central Standard Time'"
-#>
                         ForEach($Path In $PathForDeletion){
                             Write-Host $SystemDivide
                             Write-Host ("Currently processing ["+$Path+"] for files older than ["+$DeletiontDate+"] to be deleted.")
                             PSexec.exe ("\\"+$Name) cmd.exe /C ("ForFiles /P "+$Path+" /S /M *.* /C ""cmd.exe /C Del /F /Q @Path"" /D -30")
                             Write-Host $SystemDivide
                         }
-<#
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "$PShell Remove-Item ""$env:SystemRoot\Temp\*"" -Recurse"
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "$PShell Remove-Item ""$env:SystemRoot\SoftwareDistribution\Download\*"" -Recurse"
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "$PShell -ExecutionPolicy Bypass -File C:\Scripts\RemoteRegistry.ps1"
-#<#
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "$PShell -ExecutionPolicy Bypass -File C:\ClientHealth\ConfigMgrClientHealth.ps1 -Config $CFGFile -Webservice https://$SiteServer/ConfigMgrClientHealth"
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "$PShell -ExecutionPolicy Bypass -File C:\ClientHealth\ClientHealthMonitor.ps1"
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "$PShell -ExecutionPolicy Bypass -File C:\ClientHealth\Remove-WMIInvalidContent.ps1"
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "$PShell Remove-Item ""C:\ClientHealth\*.xml"" -Recurse -Force"
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "$PShell Remove-Item ""C:\ClientHealth\*.ps1"" -Recurse -Force"
->
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "UsoClient StartScan"
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "UsoClient StartDownload"
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "UsoClient StartInstall"
-                        PSexec.exe ("\\"+$Name) cmd.exe /C "UsoClient RestartDevice"
-#>
+                        $RemoteTemp=("\\"+$Name+"\C$\Scripts")
+                        $ClientHealth=@"
+@Echo Off
+REM param($SiteCode)
+If $SiteCode EQU B01 Goto SiteB
+:SiteA
+Set CFGFile=C:\ClientHealth\configa.xml
+Set SiteServer=https://w19sccmmpa01.inf.utshare.local/ConfigMgrClientHealth
+Goto Start
+:SiteB
+Set CFGFile=C:\ClientHealth\configb.xml
+Set SiteServer=https://w19sccmmpb01.inf.utshare.local/ConfigMgrClientHealth
+:Start
+Set PShell="C:\Windows\System32\WindowsPowerShell\v1.0\PowerShell.exe"
+Echo "Beginning script for checking the Client Health of the workstation." > C:\ClientHealth\RemoteCheck.log
+Echo:   "Using the "File System Utility" to remove "8.3" backward compatibility!"
+Echo "Using the 'File System Utility' to remove '8.3' backward compatibility!" >> C:\ClientHealth\RemoteCheck.log
+fsutil 8dot3name set 1
+Echo:   "Setting the PowerShell "Execution Policy" to "Unrestricted" for the CurrentUser scope."
+Echo "Setting the PowerShell 'Execution Policy' to 'Unrestricted' for the CurrentUser scope." >> C:\ClientHealth\RemoteCheck.log
+%PShell% Set-ExecutionPolicy -Scope CurrentUser Unrestricted
+Echo:   "Setting the computer's timezone to 'Central Standard Time'."
+Echo "Setting the computer's timezone to 'Central Standard Time'." >> C:\ClientHealth\RemoteCheck.log
+%PShell% Set-TimeZone -Name 'Central Standard Time'
+Echo:   "Beginning to process 'C:\Scripts\RemoteRegistry.ps1' for enabling the Remote Registry service."
+Echo "Beginning to process 'C:\Scripts\RemoteRegistry.ps1' for enabling the Remote Registry service." >> C:\ClientHealth\RemoteCheck.log
+%PShell% -ExecutionPolicy Bypass -File C:\Scripts\RemoteRegistry.ps1
+Echo:   "Beginning to process 'C:\ClientHealth\ConfigMgrClientHealth.ps1' for validating the SCCM Client installation."
+Echo "Beginning to process 'C:\ClientHealth\ConfigMgrClientHealth.ps1' for validating the SCCM Client installation." >> C:\ClientHealth\RemoteCheck.log
+%PShell% -ExecutionPolicy Bypass -File C:\ClientHealth\ConfigMgrClientHealth.ps1 -Config %CFGFile% -Webservice %SiteServer%
+Echo:   "Beginning to process 'C:\ClientHealth\ClientHealthMonitor.ps1' to get the current health of the SCCM Client."
+Echo "Beginning to process 'C:\ClientHealth\ClientHealthMonitor.ps1' to get the current health of the SCCM Client." >> C:\ClientHealth\RemoteCheck.log
+%PShell% -ExecutionPolicy Bypass -File C:\ClientHealth\ClientHealthMonitor.ps1
+Echo:   "Beginning to cleanup the temporary files used in this script."
+Echo "Beginning to cleanup the temporary files used in this script." >> C:\ClientHealth\RemoteCheck.log
+%PShell% Remove-Item ""%SystemDrive%\ClientHealth\*.xml"" -Force
+%PShell% Remove-Item ""%SystemDrive%\ClientHealth\*.ps1"" -Force
+%PShell% Remove-Item ""%SystemDrive%\Scripts\Users.tmp"" -Force
+Echo:   "Completed processing the computer's client health."
+Echo "Completed processing the computer's client health." >> C:\ClientHealth\RemoteCheck.log
+Echo:   "Kicking off Windows Security Updates Scan/Download/Install/Reboot process."
+Echo "Kicking off Windows Security Updates Scan/Download/Install/Reboot process." >> C:\ClientHealth\RemoteCheck.log
+REM UsoClient StartScan
+REM UsoClient StartDownload
+REM UsoClient StartInstall
+REM UsoClient RestartDevice
+Exit
+"@
+                        Add-Content $RemoteTemp\ClientHealth.bat $ClientHealth
+                        $RemoteScript=($env:SystemDrive+"\Scripts\ClientHealth.bat")
+                        If(Test-Connection -ComputerName $Name -Quiet){
+                            Try{
+                                If(Test-Path -Path ("\\"+$Name+"\C$\ClientHealth\RemoteCheck.log")){
+                                    Remove-Item ("\\"+$Name+"\C$\ClientHealth\RemoteCheck.log")
+                                }
+                                PSexec.exe ("\\"+$Name) cmd.exe /C $RemoteScript
+                                Write-Host ("Completed processing: ["+$RemoteScript+"] on ["+$Name+"].")
+                            }Catch{
+                                Write-Host ("Failed to process: ["+$RemoteScript+"] on ["+$Name+"].")
+                            }
+                        }
                     }Catch{
                     }
-                    Remove-Item $Destination
+                    If(Test-Path -Path ("\\"+$Name+"\C$\ClientHealth\RemoteCheck.log")){
+                        Remove-Item ($RemoteTemp+"\ClientHealth.bat")
+                        Remove-Item $Destination
+                    }
                 }
                 Write-Host $SystemDivide
-#                Pause
-#>
             }
         }
     }
---
+++ PowerShell/VM_PowerState.ps1
@@ -0,0 +1,196 @@
+﻿Clear-Host;Clear-History
+Import-Module ActiveDirectory
+Import-Module ProcessCredentials
+$Global:Domain = ($env:USERDNSDOMAIN.ToLower())
+Set-Variable -Name VCMgrSrvList -Value @('vcmgra01.inf','vcmgrb01.inf')
+$moduleList = @(
+    "VMware.VimAutomation.Core",
+    "VMware.VimAutomation.Vds",
+    "VMware.VimAutomation.Cloud",
+    "VMware.VimAutomation.PCloud",
+    "VMware.VimAutomation.Cis.Core",
+    "VMware.VimAutomation.Storage",
+    "VMware.VimAutomation.HorizonView",
+    "VMware.VimAutomation.HA",
+    "VMware.VimAutomation.vROps",
+    "VMware.VumAutomation",
+    "VMware.DeployAutomation",
+    "VMware.ImageBuilder",
+    "VMware.VimAutomation.License"
+    )
+$productName = "PowerCLI"
+$productShortName = "PowerCLI"
+$loadingActivity = ("Loading ["+$productName+"]")
+$script:completedActivities = 0
+$script:percentComplete = 0
+$script:currentActivity = ""
+$script:totalActivities = $moduleList.Count + 1
+<#
+    .SYNOPSIS
+        Gets a list of VM's dependant on power state.
+    .DESCRIPTION
+        Gets a list of VM's dependant on power state.
+    .PARAMETER Cluster
+        Name of the cluster to retrieve VM's from. Supports Wildcards.
+    .PARAMETER PowerState
+        REQUIRED. The power state of the VM's that you are looking for.
+        Valid state options are:
+            PoweredOn
+            PoweredOff
+            Suspended
+    .PARAMETER Server
+        The name or IP of the vCenter or ESX(i) host to connect to. This assumes that you have sufficient access rights as the logged on user.
+    .PARAMETER Outfile
+        Name & path to an output file.
+    .EXAMPLE
+        Get-PowerState -Cluster ClusterName -State PoweredOn -Server 127.0.0.1 -outfile filename.txt
+#>
+Function Get-VMByPowerState{[CmdletBinding(defaultparametersetname='ByName')]
+    Param(
+        [Parameter(Mandatory=$False,Position = 0,ParameterSetName='ByName')][Alias("ClusterName")][string]$Cluster = '*',
+        [Parameter(Mandatory=$true,ParameterSetName='ByDatacenter')][Alias("PowerStateEntry")][string]$PowerState,
+        [Parameter(Mandatory=$false,ParameterSetName='ByDatacenter')][Alias("vCenterHost")][string]$Server = $null,
+        [Parameter(Mandatory=$false,ParameterSetName='ByDatacenter')][Alias("Output File Name")]$outfile = $null
+    )
+    Process{
+        If($server -ne $null){
+            connect-viserver $server
+            Clear-Host
+        }
+        $vms = Get-VM|Where-Object {$_.PowerState -eq $PowerState}|Select-Object Name,PowerState
+        $vmcount = $vms.count
+        If($vmcount -gt "0"){
+            If($outfile -eq $null){
+                ""
+                Write-Host ("List of all "+$PowerState+" VM's:")
+                $vms
+                ""
+                Write-Host ("There are ["+$vmcount+"] ["+$PowerState+"] VM's")
+            }
+            If($outfile -ne $null){
+                ""
+                Write-Host "Outputting to file as requested."
+                Out-File -FilePath $outfile -InputObject $vms}
+            }
+        If($server -ne $null){
+            disconnect-viserver $server -force:$true -confirm:$false
+        }
+    }
+}
+Function LoadModules(){
+    ReportStartOfActivity ("Searching for ["+$productShortName+"] module components...")
+    $loaded = Get-Module -Name $moduleList -ErrorAction Ignore | % {$_.Name}
+    $registered = Get-Module -Name $moduleList -ListAvailable -ErrorAction Ignore | % {$_.Name}
+    $notLoaded = $registered | ? {$loaded -notcontains $_}
+    ReportFinishedActivity
+    Foreach($module in $registered){
+        If($loaded -notcontains $module){
+            ReportStartOfActivity ("Loading module ["+$module+"].")
+            Import-Module $module
+            ReportFinishedActivity
+        }
+    }
+}
+Function ReportFinishedActivity(){
+    $script:completedActivities++
+    $script:percentComplete = (100.0 / $totalActivities) * $script:completedActivities
+    $script:percentComplete = [Math]::Min(99, $percentComplete)
+    Write-Progress -Activity $loadingActivity -CurrentOperation $script:currentActivity -PercentComplete $script:percentComplete
+}
+Function ReportStartOfActivity($activity){
+    $script:currentActivity = $activity
+    Write-Progress -Activity $loadingActivity -CurrentOperation $script:currentActivity -PercentComplete $script:percentComplete
+}
+LoadModules
+Write-Progress -Activity $loadingActivity -Completed
+Switch($DomainUser){
+    {($_-like"sy100*")-or($_-like"sy600*")}{Break}
+    Default{$DomainUser=(("sy1000829946@"+$Domain).ToLower());Break}
+}
+$SecureCredentials=SetCredentials -SecureUser $DomainUser -Domain ($Domain).Split(".")[0]
+If(!($SecureCredentials)){$SecureCredentials=Get-Credential}
+Clear-History;Clear-Host
+$Global:DataEntry = ""
+[int]$FormHeight=$form.Size.Height=200
+[int]$FormWidth=$form.Size.Width=400
+[int]$LabelLeft=10
+[int]$LabelTop=20
+[int]$TextboxTop=$LabelTop*2
+[int]$ButtonTop=$FormHeight/2
+[int]$ButtonLeft=$FormWidth/4
+[int]$ButtonHeight=$FormHeight/6.5
+[int]$ButtonWidth=$FormWidth/4.5
+[int]$LabelHeight=$FormHeight/$LabelLeft
+[int]$LabelWidth=$FormWidth-($LabelLeft*4)
+Do{
+    Add-Type -AssemblyName System.Windows.Forms
+    Add-Type -AssemblyName System.Drawing
+
+    $form = New-Object System.Windows.Forms.Form
+    $form.Text = 'VM PowerState'
+    $form.Size = New-Object System.Drawing.Size($FormWidth,$FormHeight)
+    $form.StartPosition = 'CenterScreen'
+
+    $okButton = New-Object System.Windows.Forms.Button
+    $okButton.Location = New-Object System.Drawing.Point($ButtonLeft,($ButtonTop))
+    $okButton.Size = New-Object System.Drawing.Size($ButtonWidth,$ButtonHeight)
+    $okButton.Text = 'OK'
+    $okButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+    $form.AcceptButton = $okButton
+    $form.Controls.Add($okButton)
+
+    $cancelButton = New-Object System.Windows.Forms.Button
+    $cancelButton.Location = New-Object System.Drawing.Point(($ButtonLeft+$ButtonWidth),($ButtonTop))
+    $cancelButton.Size = New-Object System.Drawing.Size($ButtonWidth,$ButtonHeight)
+    $cancelButton.Text = 'Cancel'
+    $cancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+    $form.CancelButton = $cancelButton
+    $form.Controls.Add($cancelButton)
+
+    $label = New-Object System.Windows.Forms.Label
+    $label.Location = New-Object System.Drawing.Point($LabelLeft,$LabelTop)
+    $label.Size = New-Object System.Drawing.Size($LabelWidth,$LabelHeight)
+    $label.Text = 'Please enter the hostname of the computer.'
+    $form.Controls.Add($label)
+
+    $textBox = New-Object System.Windows.Forms.TextBox
+    $textBox.Location = New-Object System.Drawing.Point($LabelLeft,$TextboxTop)
+    $textBox.Size = New-Object System.Drawing.Size($LabelWidth,$LabelHeight)
+    $form.Controls.Add($textBox)
+
+    $form.Topmost = $true
+
+    $form.Add_Shown({$textBox.Select()})
+    $result = $form.ShowDialog()
+    If($result -eq [System.Windows.Forms.DialogResult]::Cancel){
+        $DataEntry = "exit"
+    }
+
+    If($result -eq [System.Windows.Forms.DialogResult]::OK){
+        $Hostname = ""
+        $DataEntry = $textBox.Text
+        Switch(($DataEntry).ToLower()){
+            {($_-eq"exit")}{
+                Break
+            }
+            {($_.Length-eq0)-or($_-contains" ")}{
+                $Message=("The value that you entered ["+$DataEntry+"] didn't return a valid active computer in the ["+$Domain+"] domain.")
+                [System.Windows.MessageBox]::Show($Message,'Hostname not found!','Ok','Error')
+                Clear-History;Clear-Host
+                Break
+            }
+            Default{
+                $Hostname=($DataEntry+"*")
+                $Hostname = (Get-ADComputer -Filter {DNSHostname -like $Hostname} -Properties Name,IPv4Address,OperatingSystem|Sort-Object Name|Select-Object DNSHostName,IPv4Address,OperatingSystem)
+                If($Hostname){
+                    ForEach($ComputerName In $Hostname){
+                        $DNSHostName = $ComputerName.DNSHostName
+                        Get-VMByPowerState
+                        Write-Host $DNSHostName
+                    }
+                }
+                Break
+            }
+        }
+    }
+}Until($DataEntry.ToLower()-eq"exit")
