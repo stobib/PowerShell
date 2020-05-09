2b2a3c9b125294d423061bad3bf4792a21e9e9b7
Bob W Stobie <bstobie@utsystem.edu>
2020-05-08 22:56:37 -0500

    Scheduled Task for Maximum Password Age

---
+++ PowerShell/ScheduledTask.ps1
@@ -0,0 +1,7 @@
+﻿[string]$Domain=(($env:USERDNSDOMAIN).Split(".")[0]).ToUpper()
+$Action=New-ScheduledTaskAction -Execute ($env:SystemDrive+"\Scripts\MonitorAge.ps1")
+$Person=New-ScheduledTaskPrincipal ($Domain+"\zasvccm_cp")
+$Trigger=New-ScheduledTaskTrigger -AtLogOn
+$Settings=New-ScheduledTaskSettingsSet
+$Create=New-ScheduledTask -Action $Action -Principal $Person -Trigger $Trigger -Settings $Settings
+Register-ScheduledTask MonitorPasswordAge -InputObject $Create
