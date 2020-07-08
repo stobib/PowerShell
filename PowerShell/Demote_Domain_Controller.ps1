2761a44722c591a611fdccbaa7aa0c148b16d79f
Bob W Stobie <bstobie@utsystem.edu>
2020-07-08 09:54:12 -0500

    Initial Script

---
+++ PowerShell/Demote_Domain_Controller.ps1
@@ -0,0 +1,6 @@
+﻿#
+# Windows PowerShell script for AD DS Deployment
+#
+
+Import-Module ADDSDeployment
+Uninstall-ADDSDomainController -DemoteOperationMasterRole:$true -DnsDelegationRemovalCredential (Get-Credential) -RemoveDnsDelegation:$true -Force:$true
