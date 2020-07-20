c04003ea025470f919ba8ffc918199cb0cb1bbc6
Bob W Stobie <bstobie@utsystem.edu>
2020-07-20 06:51:25 -0500

    New script

---
+++ PowerShell/lastlogon.ps1
@@ -0,0 +1,8 @@
+﻿Clear-History;Clear-Host
+Set-Variable -Name AdminShare -Value \\utshare.local\cifs\SysAdmins
+Set-Variable -Name FilePath -Value ($AdminShare+"\ADUsers")
+Set-Variable -Name FileName -Value ($FilePath+"\enabled_accounts.csv")
+If(!(Test-Path -Path $FilePath)){New-Item -Path $AdminShare -Name "ADUsers" -ItemType Directory}
+Get-ADUser -Filter {enabled -eq $true} -Properties LastLogonTimeStamp |
+Select-Object Name,@{Name="Stamp"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp).ToString('yyyy-MM-dd_hh:mm:ss')}} |
+Sort-Object Name | Out-File -FilePath $FileName -NoClobber
